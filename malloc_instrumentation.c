13f9683209a5a77b689b4dd8b4a6741bef4b9c0a
Nicola <mazzun@usi.ch>
2024-03-22 09:06:23 +0100

    first attempt at allocation checking

--- malloc_instrumentation.c
+++ malloc_instrumentation.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+
 #include <dlfcn.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -6,7 +7,26 @@
 #include <unistd.h>
 #include <string.h>
 
+// TODO: check if problematic for forked tests?
 static size_t max_allocations = 1;
+
+#define SET_ALLOC(value) max_allocations = value
+
+#define CHECK_ALLOC(expected) do {  \
+	if ((expected) != max_allocations) {			\
+	    printf("\n%s:%d: Allocation assertion failed\n     expected '"#expected"'; found %lu\n", \
+		   __FILE__, __LINE__, max_allocations);	\
+	    return 0;					                \
+	}	                                            \
+    } while (0)
+
+int *dummy_test() {
+    SET_ALLOC(1);
+    int *p = malloc(sizeof(int));
+    CHECK_ALLOC(0);
+    free(p);
+    CHECK_ALLOC(1);
+}
 
 int main(void)
 {
@@ -18,12 +38,17 @@ int main(void)
 	free(p);
     } else write(STDOUT_FILENO, "Allocation failed\n", strlen("Allocation failed\n"));
 
+    int *c = malloc(sizeof(int));
+    if (c) {
+	write(STDOUT_FILENO, "Unexpectedly Allocated\n", strlen("Unexpectedly  Allocated\n"));
+    } else write(STDOUT_FILENO, "Allocation failed\n", strlen("Allocation failed\n"));
+
     int *s = malloc(sizeof(int));
     assert(!s);
+
+    dummy_test();
     
 }
-
-
 
 void *malloc(size_t size)
 {
@@ -38,4 +63,16 @@ void *malloc(size_t size)
     
     return 0;
 
+}
+
+void free(void * ptr)
+{ 
+    static void (*libc_free)(void *) = NULL;
+    if (!libc_free) 
+	libc_free = dlsym(RTLD_NEXT, "free");	
+
+    if (max_allocations > 0) {
+	++max_allocations;
+	return libc_free(ptr);
+    }
 }
